3fbfef5d0851724ebd217c9e3362ec8c7625ce3a
Julio Diniz <julio.dinizk1@gmail.com>
2019-01-25 15:05:28 -0200

    Andamento até a video aula 3

---
+++ src/components/global/BaseSpinner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="base-spinner fa-3x">
+    <i class="fas fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables";
+  .base-spinner{
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: $dark-medium;
+    color: $feature;
+    z-index: 100;
+  }
+</style>
